749868ca5b2e5ce536d221190ce93e5337425092
sitannino25 <tannino.2043951@studenti.uniroma1.it>
2025-05-20 10:37:42 +0200

    Update init.sql

--- src/mariadb_init/init.sql
+++ src/mariadb_init/init.sql
@@ -53,7 +53,6 @@ create table if not exists question_answer(
     question_id int not null,
     answer_id int not null,
     points int default 0,
-    
     primary key(question_id,answer_id),
     foreign key(question_id) references question(question_id) on delete cascade on update cascade,
     foreign key(answer_id) references answer(answer_id) on delete cascade on update cascade
@@ -63,7 +62,8 @@ create table if not exists user_answer_question(
     user_id int not null,
     answer_id int not null,
     question_id int not null,
-    primary key(user_id,answer_id,question_id),
+    answered_at timestamp default CURRENT_TIMESTAMP
+    primary key(user_id,question_id),
     foreign key(question_id,answer_id) references question_answer(question_id,answer_id) on delete cascade on update cascade,
     foreign key(user_id) references user(user_id) on delete cascade on update cascade
 );
