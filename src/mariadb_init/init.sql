d4e30037db89fa526b206a8f00d6a947f12fd5c2
sitannino25 <tannino.2043951@studenti.uniroma1.it>
2025-06-28 20:37:31 +0200

    Update init.sql

--- src/mariadb_init/init.sql
+++ src/mariadb_init/init.sql
@@ -20,13 +20,16 @@ insert into avatar(path) values(
 create table if not exists user(
     user_id int AUTO_INCREMENT primary key,
     username varchar(255) unique not null,
-    name varchar(255) not null,
-    surname varchar(255) not null,
+    email varchar(255) unique not null,
+    name varchar(255),
+    surname varchar(255),
     bio text,
     current_avatar_id int default 1,
     password varchar(255) not null,
+    salt text not null,
     user_points int default 0,
-    --user_points_season int default 0,
+    custom_profile_image boolean default 0,    
+    /*--user_points_season int default 0,*/
     user_coins int default 0,
     created_at timestamp default CURRENT_TIMESTAMP,
     last_login_at timestamp,
@@ -47,7 +50,7 @@ create table if not exists title(
 create table if not exists mission(
     mission_id int AUTO_INCREMENT primary key,
     type ENUM('daily','weekly','objective'),
-    kind ENUM('answer','question'),        --indica su cosa va a contare il valore (es: rispondi a 10 domande -> kind=answer , crea 10 domande sul calcio -> kind=question theme=calcio)
+    kind ENUM('answer','question'),        /*--indica su cosa va a contare il valore (es: rispondi a 10 domande -> kind=answer , crea 10 domande sul calcio -> kind=question theme=calcio)*/
     theme int,
     description text not null,
     reward_coins int not null,
@@ -100,6 +103,10 @@ create table if not exists llm(
 );
 
 
+insert into llm (name) values ("gemma3:4b"),("gemma3:1b");
+
+
+
 create table if not exists lingua(
     lingua_id int AUTO_INCREMENT primary key,
     name varchar(255)
@@ -107,26 +114,18 @@ create table if not exists lingua(
 
 create table if not exists question(
     question_id int AUTO_INCREMENT primary key,
-    question_tags text not null,
-    question_text varchar(500) not null unique,
-    created_by_user_id int,
-    created_by_llm_id int,
+    /*question_tags text not unique null,*/
+    question_text varchar(500) not null,
+    created_by_user_id int default null,
+    created_by_llm_id int default null,
     created_at timestamp default CURRENT_TIMESTAMP,
     rankings_times int default 0,
-    --answers_number int default 0, si può fare con count *
+    /*--answers_number int default 0, si può fare con count **/
     status ENUM('open','close') default 'open',
     foreign key(created_by_user_id) references user(user_id) on update cascade on delete set null,
     foreign key(created_by_llm_id) references llm(llm_id) on update cascade on delete set null
-    --difficulty ENUM('hard','medium','easy'),  può valutarla un LLM?
-    --target_culture varchar(50) default 'italiana' tabella lingua?
-);
-
-create table if not exists answer(
-    answer_id int AUTO_INCREMENT primary key,
-    answer_tags text not null,
-    answer_text text not null,
-    created_at timestamp default CURRENT_TIMESTAMP
-    --is_culturally_specific_response boolean
+    /*--difficulty ENUM('hard','medium','easy'),  può valutarla un LLM?
+    --target_culture varchar(50) default 'italiana' tabella lingua?*/   
 );
 
 create table if not exists report_type(
@@ -158,47 +157,28 @@ create table if not exists question_theme(
     foreign key (theme_id) references theme(theme_id) on delete cascade on update cascade 
 );
 
-create table if not exists question_answer(
+
+create table if not exists answer(
+    answer_id int AUTO_INCREMENT primary key,
+    llm_id int default null,
+    user_id int default null,
     question_id int not null,
-    answer_id int not null,
+    answered_at timestamp default CURRENT_TIMESTAMP,
+    answer_text text not null,
     points int default 0,
-    primary key(question_id,answer_id),
+    foreign key(user_id) references user(user_id) on update cascade on delete set null,
     foreign key(question_id) references question(question_id) on delete cascade on update cascade,
-    foreign key(answer_id) references answer(answer_id) on delete cascade on update cascade
-);
-
-create table if not exists llm_answer_question(
-    llm_id int not null,
-    answer_id int not null,
-    question_id int not null,
-    answered_at timestamp default CURRENT_TIMESTAMP,
-    primary key(llm_id,question_id,answer_id),
-    foreign key(question_id,answer_id) references question_answer(question_id,answer_id) on delete cascade on update cascade,
-    foreign key(llm_id) references llm(llm_id) on delete cascade on update cascade
+    foreign key(llm_id) references llm(llm_id) on update cascade on delete set null
 );
 
 
-create table if not exists user_answer_question(
-    user_id int not null,
-    answer_id int not null,
-    question_id int not null,
-    answered_at timestamp default CURRENT_TIMESTAMP,
-    primary key(user_id,question_id),
-    foreign key(question_id,answer_id) references question_answer(question_id,answer_id) on delete cascade on update cascade,
-    foreign key(user_id) references user(user_id) on delete cascade on update cascade
-);
-
-
-create table if not exists rating_llm_answer_question_user(
+create table if not exists rating_llm_answer(
     rating_id int AUTO_INCREMENT primary key,
-    llm_id int not null,
     answer_id int not null,
-    question_id int not null,
     user_id int not null,
-    unique(llm_id,answer_id,question_id,user_id),
     value int not null,
-    foreign key(user_id) references user(user_id) on delete cascade on update cascade,
-    foreign key(llm_id,question_id,answer_id) references llm_answer_question(llm_id,question_id,answer_id) on delete cascade on update cascade
+    foreign key(user_id) references user(user_id) on delete cascade on update cascade, 
+    foreign key(answer_id) references answer(answer_id) on delete cascade on update cascade
 );
 
 
